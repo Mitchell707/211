40d5c2c76e41107bd51a13b421a1087ed3021848
Mitchell707 <31227154+Mitchell707@users.noreply.github.com>
2018-11-27 18:38:01 -0800

    Update square.cpp

---
+++ 211/p6/square.cpp
@@ -0,0 +1,32 @@
+//square.cpp
+//Beck, Mitchell
+//mbeck16
+
+#include <iostream>
+#include <string>
+#include "square.h"
+#include "grid.h"
+#include "shape.h"
+
+using namespace std;
+
+void Square::draw(Grid &grid)
+{
+    size = size - 1;
+
+    //grid.arr[x][y] = "*";
+    grid.set(x + size, y + size, '*');
+
+    for(int i = x; i < x + size; i++)//draws two lines along the x axis size length
+    {
+        grid.set(i, y, '*');
+        grid.set(i, y + size, '*');
+    }
+
+    for(int i = y; i < y + size; i++)//draws two lines along the y axis size length
+    {
+        grid.set(x, i, '*');
+        grid.set(x + size, i, '*');
+    }
+    
+}
