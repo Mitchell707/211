27970bb570843050220d70183c25e33a0e3106d7
Mitchell707 <31227154+Mitchell707@users.noreply.github.com>
2018-11-27 18:36:37 -0800

    Update shape.cpp

---
+++ 211/p6/shape.cpp
@@ -0,0 +1,16 @@
+//shape.cpp
+//Beck, Mitchell
+//mbeck16
+
+#include <iostream>
+#include <string>
+#include "shape.h"
+
+using namespace std;
+
+Shape::Shape(int a, int b)
+{
+    x = a;
+    y = b;
+
+}
